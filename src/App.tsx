e2afdde3a5edfca87d816f179e49d74a78340af9
Josh McCarthy <joshcaterham@gmail.com>
2023-04-24 15:56:49 +0200

    feat: app.tsx setup and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import { GroupTasks } from './features/index';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <div className="container">
+          <GroupTasks />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
