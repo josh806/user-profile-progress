41942e3d6d7d6121c6aa1aecd0b1eefa99d12faf
Josh McCarthy <joshcaterham@gmail.com>
2023-04-25 22:05:30 +0200

    fix: round progress percentage UP to nearest whole percent

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export function calculatePercentage(valuesTotal: number, valueToAdd = 0) {
-  return Math.round((valueToAdd * 100) / valuesTotal);
+  return Math.ceil((valueToAdd * 100) / valuesTotal);
 }
