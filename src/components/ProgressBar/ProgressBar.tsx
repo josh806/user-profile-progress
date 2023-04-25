51f8849d1c9130b4b7f27234d4189311964170c7
Josh McCarthy <joshcaterham@gmail.com>
2023-04-25 21:12:26 +0200

    feat: add ProgressBar

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import './ProgressBar.css';
+
+type Props = {
+  bgColor?: string;
+  percentage: number;
+};
+
+export default function ProgressBar({ bgColor, percentage }: Props) {
+  const progressBarStyles = {
+    backgroundColor: bgColor,
+    width: `${percentage}%`,
+  };
+
+  const progressBarLabelStyles = {
+    position: 'absolute',
+    left: 'calc(100% + 0.5rem)',
+    color: 'inherit',
+  };
+
+  return (
+    <div className="progress-bar">
+      <div className="progress-bar__inner" style={progressBarStyles}>
+        <span
+          className="progress-bar__label"
+          style={percentage < 10 ? progressBarLabelStyles : {}}
+        >{`${percentage}%`}</span>
+      </div>
+    </div>
+  );
+}
